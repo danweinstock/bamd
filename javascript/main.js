8adfcb6ab4e9c9b67ac316c6398017a1e9bd0b7f
danweinstock <dan.weinstock@yahoo.com>
2016-08-07 14:31:04 -0400

    flickity carousel added for about us section and flex added for the rest of the sections

--- javascript/main.js
+++ javascript/main.js
@@ -110,3 +110,15 @@ $(document).ready(function(){
 			event.preventDefault();
 	});
 });
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
